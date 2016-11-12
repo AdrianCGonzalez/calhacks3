823506da32248b4ab55a475890a23bab59ed7ca3
EstebanMagallonPerez <esteban1995perez@gmail.com>
2016-11-12 12:02:23 -0800

    '

--- aboutUs.php
+++ aboutUs.php
@@ -135,34 +135,25 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-panel">
+                <div class="row">
+                    <div class ="col s12 m4">
+                        <div class="card-panel">
 
-                    <div class="row">
-                        <div class="col s12 m4" id="description1">
-                            <h1 class="padMe">Bar Chart</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor eros eget mauris blandit accumsan. Suspendisse accumsan blandit commodo. Nulla lobortis metus nec egestas luctus. Nunc ac hendrerit massa, vel blandit mauris. Vestibulum feugiat porta varius. Ut feugiat arcu purus, vitae dignissim erat finibus vel. Donec eu arcu sit amet dui cursus semper. In hac habitasse platea dictumst. Mauris ornare non quam non vehicula. Nunc molestie sagittis ex eget placerat.</p>
-                        </div>
-                        <div class="col s12 m8" id="myChartcon"  height="50px">
-                            <div class="ct-chart ct-perfect-fourth" id="myPi"></div>
-                            <script>makepi("#myPi");</script>
-                        </div>
-                    </div>
+                            <div class="row">
+                                <div class="col s12 m4" id="description1">
+                                    <h1 class="padMe">Bar Chart</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor eros eget mauris blandit accumsan. Suspendisse accumsan blandit commodo. Nulla lobortis metus nec egestas luctus. Nunc ac hendrerit massa, vel blandit mauris. Vestibulum feugiat porta varius. Ut feugiat arcu purus, vitae dignissim erat finibus vel. Donec eu arcu sit amet dui cursus semper. In hac habitasse platea dictumst. Mauris ornare non quam non vehicula. Nunc molestie sagittis ex eget placerat.</p>
+                                </div>
+                                <div class="col s12 m8" id="myChartcon"  height="50px">
+                                    <div class="ct-chart ct-perfect-fourth" id="myPi"></div>
+                                    <script>makepi("#myPi");</script>
+                                </div>
+                            </div>
 
-                </div>
-                <div class="card-panel">
-
-                    <div class="row">
-                        <div class="col s12 m4" id="description1">
-                            <h1 class="padMe">Bar Chart</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor eros eget mauris blandit accumsan. Suspendisse accumsan blandit commodo. Nulla lobortis metus nec egestas luctus. Nunc ac hendrerit massa, vel blandit mauris. Vestibulum feugiat porta varius. Ut feugiat arcu purus, vitae dignissim erat finibus vel. Donec eu arcu sit amet dui cursus semper. In hac habitasse platea dictumst. Mauris ornare non quam non vehicula. Nunc molestie sagittis ex eget placerat.</p>
-                        </div>
-                        <div class="col s12 m8" id="myChartcon"  height="50px">
-                            <div class="ct-chart ct-perfect-fourth" id="myChart"></div>
-                            <script>makeChart("#myChart");</script>
                         </div>
                     </div>
-
                 </div>
+
 
                 <div class="card-panel">
                     <div class="row">
@@ -205,11 +196,30 @@
                                     </tr>
                                 </tbody>
                             </table>
-
+                            <div class="col s12 right"><a class="waves-effect waves-light btn right">View Full Payment History</a></div>
                         </div>
 
                     </div>
                 </div>
+
+
+                <div class="card-panel">
+
+                    <div class="row">
+                        <div class="col s12 m4" id="description1">
+                            <h1 class="padMe">Bar Chart</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor eros eget mauris blandit accumsan. Suspendisse accumsan blandit commodo. Nulla lobortis metus nec egestas luctus. Nunc ac hendrerit massa, vel blandit mauris. Vestibulum feugiat porta varius. Ut feugiat arcu purus, vitae dignissim erat finibus vel. Donec eu arcu sit amet dui cursus semper. In hac habitasse platea dictumst. Mauris ornare non quam non vehicula. Nunc molestie sagittis ex eget placerat.</p>
+                        </div>
+                        <div class="col s12 m8" id="myChartcon"  height="50px">
+                            <div class="ct-chart ct-perfect-fourth" id="myChart"></div>
+                            <script>makeChart("#myChart");</script>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
                 <div class="card-panel">
                     <div class="row">
                         <div class="col m2 hide-on-small-only">
