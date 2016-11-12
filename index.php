5f45a61905dc2c73b80d52f13234273c94e012ba
EstebanMagallonPerez <esteban1995perez@gmail.com>
2016-11-12 01:23:58 -0800

    --

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name='viewport' content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0"/>
-		<!--		<meta http-equiv="refresh" content="5"> -->
+<!--		<meta http-equiv="refresh" content="5"> -->
 		<meta name="apple-touch-fullscreen" content="yes" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<title>Vacation</title>
@@ -18,49 +18,61 @@
 		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+      <script>
+        $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+      </script>
 
 	</head>
 
 	<body>
-		<nav>
-			<div class="nav-wrapper">
-				<a href="#" class="brand-logo"> CalHacks</a>
-				<ul id="nav-mobile" class="right hide-on-med-and-down">
+      			<nav>
+				<div class="nav-wrapper">
+					<a href="#" class="brand-logo"> CalHacks</a>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li><a href="sass.html">Sass</a></li>
+						<li><a href="badges.html">Components</a></li>
+						<li><a href="collapsible.html">JavaScript</a></li>
+					</ul>
+                  <ul id="nav-mobile" class="side-nav">
 					<li><a href="sass.html">Sass</a></li>
-					<li><a href="badges.html">Components</a></li>
-					<li><a href="collapsible.html">JavaScript</a></li>
+						<li><a href="badges.html">Components</a></li>
+						<li><a href="collapsible.html">JavaScript</a></li>
 				</ul>
-			</div>
-		</nav>
-
-		<div id="index-banner" class="parallax-container">
-
-			<div class="section no-pad-bot">
-				<div class="container hide-on-med-and-up">
-					<br>
-					<br>
-					<div style="background-color: rgba(25, 25, 25, 0.5);">
-						<h1 class="header center white-text text-lighten-2">CalHacks Team C14</h1>
-
-					</div>
-					<br>
+				<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
 
 				</div>
-				<div class="container hide-on-small-only">
-					<br>
-					<br>
-					<div style="background-color: rgba(25, 25, 25, 0.5);">
-						<h1 class="header center white-text text-lighten-2">CalHacks</h1>
-						<div class="row center">
-							<h4 class="header col s12 light">Team C14</h4>
-						</div>
-					</div>
-					<br>
+			</nav>
+
+            <div id="index-banner" class="parallax-container">
+
+              <div class="section no-pad-bot">
+			<div class="container hide-on-med-and-up">
+				<br>
+				<br>
+				<div style="background-color: rgba(25, 25, 25, 0.5);">
+					<h1 class="header center white-text text-lighten-2">CalHacks Team C14</h1>
 
 				</div>
+				<br>
+
 			</div>
-			<div class="parallax"><img src="https://inst.eecs.berkeley.edu/~cs194-26/fa14/upload/files/proj3/cs194-fb/images/blurry_turtle.jpg" alt="Unsplashed background img 1"></div>
+			<div class="container hide-on-small-only">
+				<br>
+				<br>
+				<div style="background-color: rgba(25, 25, 25, 0.5);">
+					<h1 class="header center white-text text-lighten-2">CalHacks</h1>
+					<div class="row center">
+						<h4 class="header col s12 light">Team C14</h4>
+					</div>
+				</div>
+				<br>
+
+			</div>
 		</div>
+		<div class="parallax"><img src="https://inst.eecs.berkeley.edu/~cs194-26/fa14/upload/files/proj3/cs194-fb/images/blurry_turtle.jpg" alt="Unsplashed background img 1"></div>
+	</div>
 
 		<div class="contentWrapper">
 
@@ -68,10 +80,10 @@
 			<div class="pageWrapper">
 				<div>
 
-					<div class="holder repeat">
-						<div class="a f afr  card-panel">
-							<div>
-								<span><pre>
+                      <div class="holder repeat">
+					<div class="a f afr  card-panel">
+						<div>
+                          <span><pre>
                           Inspiration
                               We all have seen those traveling ads where they promise you affordable vacations to paradise on earth. Then you click, and then it turns out is not affordable at all. This is great prose. We have the bestest english. No more.
                           What it does
@@ -83,9 +95,9 @@
                           Accomplishments that we're proud of
                           	We ate a lot of bananas
                             </pre></span>
-							</div>
 						</div>
 					</div>
+				</div>
 
 				</div>
 				<div class="holder repeat">
@@ -119,7 +131,7 @@
 			<div class="footer-copyright">
 				<div class="container center">
 					<span class="hide-on-small-only">Powered by: nessie, aMaDEUS, Microsoft Azure Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">CodeForFood</a> </span>©2016 Code For Food<span class="hide-on-med-and-down">All rights reserved.</span>
-				</div>
+              </div>
 			</div>
 		</footer>
 	</body>
@@ -186,5 +198,10 @@
 		initializeAnimation();
 		var myScroll = $(this).scrollTop();
 		inAnimate(myScroll);
+        $(window).resize(function(){
+			initializeAnimation();
+			var myScroll = $(this).scrollTop();
+			inAnimate(myScroll);
+		});
 	</script>
 </html>
