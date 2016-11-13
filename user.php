35de1ede99d96ac0664acecced1fa385d964d97a
Afropenguin <mbrooks8@ucmerced.edu>
2016-11-13 11:35:29 -0800

    Update user.php

--- user.php
+++ user.php
@@ -3,7 +3,6 @@
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name='viewport' content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0"/>
-        <meta http-equiv="refresh" content="45">
         <meta name="apple-touch-fullscreen" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <title>Vacation</title>
