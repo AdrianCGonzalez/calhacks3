791429c90bda478643c605f25ffb009bfca0e756
EstebanMagallonPerez <esteban1995perez@gmail.com>
2016-11-13 00:47:44 -0800

    '

--- index.php
+++ index.php
@@ -146,8 +146,12 @@
                     <div class="col s12 m7">
                         <h1 style="padding-bottom:10px" >Personalized Traveling Based On Your Income</h1>
 
-                        <p>Nulla id ligula tortor. Quisque tincidunt neque purus, et aliquet arcu ullamcorper eu. Cras in arcu ac urna ultrices bibendum. Vestibulum rutrum diam leo, vitae pulvinar turpis facilisis ut. Morbi a ullamcorper quam. Praesent placerat ac augue vitae cursus.
-                            <br>Maecenas semper nisi nibh, in maximus magna condimentum vitae. Curabitur nec vestibulum ex, in fringilla arcu. Curabitur nec elit eget tortor tristique fringilla in in massa. Suspendisse et porttitor est, in volutpat lorem. Nulla pulvinar quis velit quis facilisis. Sed leo dolor, feugiat ac est sed, semper mattis lectus. Maecenas suscipit ornare erat sed congue. Nullam volutpat neque id libero mattis scelerisque. Integer ac ante finibus, ultricies ante id, convallis lorem. Fusce rhoncus eros vitae magna tempor, sit amet hendrerit dui aliquet.</p>
+                        <span>The world is a book, and those who do not travel read only a page.
+Of course, if books were as expensive as traveling is, they would be 100000%  more expensive (don't quote us on this). Nevertheless, traveling IS a lot of stress. It requires possibly coordinating vacation time for multiple people, school, money, responsibilities, children, and a mirad of things that can go wrong.
+	Nevertheless, our entirly innacurate polls with few data points show
+that traveling is still shows promingtly as the dream for a significant portion of Americans. Our objective is to make the process of planning this traveling easier. Using your bank information to extrapolate information about your socioeconomical status, we are able to give recommendations for places that you can afford to go and have a good time without worrying too much about money.	 Don't worry though, we know sometimes life gets rough and SF train station may not be your dream destination. We include a way for you to set a goal, and using top machine learning algorithms (again, don't quote us) we produce a realistic timeline and recommendations that will allow you to achieve your dream vacation. We are Dreamcatcher. Thank you for your time :).
+
+</span>
                     </div>
                     <div class="col s12 m5">
                         <img style="border-radius: 17px;" class="responsive-img" src="https://upload.wikimedia.org/wikipedia/commons/1/1c/Squirrel_posing.jpg">
