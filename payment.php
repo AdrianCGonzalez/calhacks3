20363f762e9bea72d3d17007db24945381bcaa49
alprusa <aprusa@ucsc.edu>
2016-11-13 01:24:38 -0800

    payments.php text change

--- payment.php
+++ payment.php
@@ -82,16 +82,15 @@
                         <h3 id="introduction"></h3>
                         <p id="balance"></p>
                     </div>
-                    <div class="row">
-                        <div class="col l3 m6 s12">
+                     <div class="col l3 m6 s12">
                             <div class="card blue-grey darken-1">
                                 <div class="card-content white-text">
                                     <span class="card-title">Payments</span>
-                                    <p>I am a very simple card. I am good at containing small bits of information.
-                                        I am convenient because I require little markup to use effectively.</p>
+                                    <p>Get updates on your payment habits. Get a detailed destricption of how well you
+                                        manage your accounts.</p>
                                 </div>
                                 <div class="card-action">
-                                    <a href="payment.php">This is a link</a>
+                                    <a href="payment.php">See Recent Payments</a>
 
                                 </div>
                             </div>
@@ -103,10 +102,9 @@
                                     <span class="card-title">Pay Bill</span>
                                     <p>Bill Pay is quicker and easier than writing and mailing paper checks.
                                         Pay your bills online in minutes!</p>
-                                    <br>
                                 </div>
                                 <div class="card-action">
-                                    <a href="#payBill.php">This is a link</a>
+                                    <a href="#payBill.php">Pay Bills Online</a>
 
                                 </div>
                             </div>
@@ -116,11 +114,10 @@
                             <div class="card blue-grey darken-1">
                                 <div class="card-content white-text">
                                     <span class="card-title">Transfer</span>
-                                    <p>I am a very simple card. I am good at containing small bits of information.
-                                        I am convenient because I require little markup to use effectively.</p>
+                                    <p>Send your money to other accounts safely and with ease. It's never been easier.</p>
                                 </div>
                                 <div class="card-action">
-                                    <a href="transfer.php">This is a link</a>
+                                    <a href="transfer.php">Transfer Account Money</a>
 
                                 </div>
                             </div>
@@ -130,11 +127,11 @@
                             <div class="card blue-grey darken-1">
                                 <div class="card-content white-text">
                                     <span class="card-title">Travel</span>
-                                    <p>I am a very simple card. I am good at containing small bits of information.
-                                        I am convenient because I require little markup to use effectively.</p>
+                                    <p>Get info an trips that you can or could afford this year based on
+                                        your account payments and activities.</p>
                                 </div>
                                 <div class="card-action">
-                                    <a href="travel.php">This is a link</a>
+                                    <a href="travel.php">Go Abroad</a>
 
                                 </div>
                             </div>
