9c43220736b994879cd9f1553c655adce11f64ae
Afropenguin <mbrooks8@ucmerced.edu>
2016-11-12 23:50:36 -0800

    Add files via upload

--- travel.php
+++ travel.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -21,13 +20,18 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/chartist/0.10.1/chartist.min.js"></script>
         <script src="js/animatedChart.js"></script>
         <script src="js/piChart.js"></script>
-
+        <script src="js/axis.js"></script>
+        <script src="js/regression.js"></script>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
         <script>
             $('.button-collapse').sideNav();
             $(document).ready(function(){
                 $('.parallax').parallax();
+            });
+            $(document).ready(function(){
+                // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+                $('.modal-trigger').leanModal();
             });
         </script>
 
@@ -48,22 +52,22 @@
     </style>
 
     <body>
-         <div class="contentWrapper">
-            <div class="pageWrapper">
+
         <nav class="teal darken-2">
             <div class="nav-wrapper">
-
-                <a href="index.php" class="brand-logo"> <img src="http://www.calhacks.io/assets/img/sittingbear.png" style="margin-top:13px;width:36px;height:36px"></a>
-                <a href="index.php" class="brand-logo hide-on-med-and-down" style="margin-left:40px">CalHacks</a>
-                <ul id="nav-mobile" class="right hide-on-small-only">
-                    <li><a href="aboutUs.php">About Us</a></li>
-                    <li><a href="index.php"><i class="material-icons">power_settings_new</i></a></li>
-                </ul>
+                <div class="contentWrapper">
+                    <div class="pageWrapper">
+                        <a href="index.php" class="brand-logo"> <img src="http://www.calhacks.io/assets/img/sittingbear.png" style="margin-top:13px;width:36px;height:36px"></a>
+                        <a href="index.php" class="brand-logo hide-on-med-and-down" style="margin-left:40px">CalHacks</a>
+                        <ul id="nav-mobile" class="right hide-on-small-only">
+                            <li><a href="aboutUs.php">About Us</a></li>
+                            <li><a href="index.php"><i class="material-icons">power_settings_new</i></a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-
         </nav>
-           </div>
-      </div>
+
         <div class="fixed-action-btn hide-on-med-and-up">
             <a class="btn-floating btn-large red">
                 <i class="large material-icons">menu</i>
@@ -73,168 +77,30 @@
                 <li><a class="btn-floating green" href="logIn.html"><i class="material-icons">power_settings_new</i></a></li>
             </ul>
         </div>
+
         <div class="contentWrapper">
             <div class="pageWrapper">
+                <div class="row">
+                    <div class="col l9 m12 card-panel">
+<p>Places you can go</p>
+                        
+                        <h3> Name Of Place </h3>
+                        <h4>$1000.00</h4>
+<p>Airline:</p>     <p>xxxxx</p>
+                        <p> depart from:</p> <p>xxxxxxx</p>
+                        <p>Description:</p> <p> THis place is great cause its great. Words words words words words Nunc ullamcorper lacus nec mi porta, et feugiat diam tempus. Donec bibendum eros nisl, eget vestibulum enim suscipit pulvinar. Sed eget tempor libero. Vestibulum accumsan sed purus in congue. Proin finibus eleifend dolor in efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed mauris f</p>
 
-
-                <div class="card-panel">
-                    <div class="col s12">
-                        <h2 id="introduction"></h2>
-                        <p id="balance"></p>
                     </div>
-                    <div class="row">
-                        <div class="col l3 m6 s12">
-                            <div class="card blue-grey darken-1">
-                                <div class="card-content white-text">
-                                    <span class="card-title">Payments</span>
-                                    <p>I am a very simple card. I am good at containing small bits of information.
-                                        I am convenient because I require little markup to use effectively.</p>
-                                </div>
-                                <div class="card-action">
-                                    <a href="#">This is a link</a>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col l3 m6 s12">
-                            <div class="card blue-grey darken-1">
-                                <div class="card-content white-text">
-                                    <span class="card-title">Pay Bill</span>
-                                    <p>Bill Pay is quicker and easier than writing and mailing paper checks.
-                                      Pay your bills online in minutes!</p>
-                                  <br>
-                                </div>
-                                <div class="card-action">
-                                    <a href="#">This is a link</a>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col l3 m6 s12">
-                            <div class="card blue-grey darken-1">
-                                <div class="card-content white-text">
-                                    <span class="card-title">Transfer</span>
-                                    <p>I am a very simple card. I am good at containing small bits of information.
-                                        I am convenient because I require little markup to use effectively.</p>
-                                </div>
-                                <div class="card-action">
-                                    <a href="#">This is a link</a>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col l3 m6 s12">
-                            <div class="card blue-grey darken-1">
-                                <div class="card-content white-text">
-                                    <span class="card-title">Travel</span>
-                                    <p>I am a very simple card. I am good at containing small bits of information.
-                                        I am convenient because I require little markup to use effectively.</p>
-                                </div>
-                                <div class="card-action">
-                                    <a href="#">This is a link</a>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class ="col s12 m4">
-                    <div class="card-panel">
-                        <div class="ct-chart ct-perfect-fourth" id="myPi"></div>
-
+<div class="col l3 m12 card-panel">
+                    
+                    
                     </div>
                 </div>
 
 
-                <div class="card-panel">
 
-                    <div class="row">
-                        <div class="card-panel">
 
-                            <div class="col s12 m2" id="myChartcon"  style="height:50px">
-                                <div class="ct-chart ct-perfect-fourth" id="myChart" style="height:50px"></div>
-                                <script>makepi("#myChart");</script>
-                            </div>
-                        </div>
 
-                        <div class="col s12 m10">
-                            <p>Deposits</p>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th data-field="id">Amount</th>
-                                        <th data-field="name">Transaction Date</th>
-
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <tr>
-                                        <td>Alvin</td>
-                                        <td>Eclair</td>
-
-                                    </tr>
-                                    <tr>
-                                        <td>Alan</td>
-                                        <td>Jellybean</td>
-
-                                    </tr>
-
-                                </tbody>
-                            </table>
-
-                        </div>
-                        <div class="col s12 right"><a href="#"  class="waves-effect waves-light btn right">View Full Purchace History</a></div>
-                    </div>
-                </div>
-
-                <div class="card-panel">
-                    <div class="row">
-                        <div class="card-panel">
-
-                            <div class="col s12 m2" id="myChartcon">
-                                <div class="ct-chart ct-perfect-fourth" id="myChart1"></div>
-                                <script>makepi("#myChart1");</script>
-                            </div>
-                        </div>
-
-                        <div class="col s12 m10">
-                            <p>Purchaces</p>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th data-field="id">Description</th>
-                                        <th data-field="price">Item Price</th>
-                                        <th data-field="date">Date Purchased</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <tr>
-                                        <td>Alvin</td>
-                                        <td>Eclair</td>
-                                        <td>$0.87</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Alan</td>
-                                        <td>Jellybean</td>
-                                        <td>$3.76</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Jonathan</td>
-                                        <td>Lollipop</td>
-                                        <td>$7.00</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="col s12 right"><a href="#"  class="waves-effect waves-light btn right">View Full Deposit History</a></div>
-                    </div>
-                </div>
             </div>
         </div>
         <footer class="page-footer teal darken-2">
@@ -264,150 +130,4 @@
 
     </body>
 
-
-
-
-    <script>
-        function initializeAnimation()
-        {
-            $( ".holder" ).each(function() {
-                var myElement=$(this);
-                if(myElement.children().hasClass("afl")||myElement.children().hasClass("afr"))
-                {
-                    myElement.height(myElement.children().outerHeight()+"px");
-                }
-            });
-        }
-        $(document).on('scroll', function() {
-            var myScroll = $(this).scrollTop();
-            inAnimate(myScroll);
-        });
-
-        function inAnimate(myScroll)
-        {
-            $( ".holder" ).each(function() {
-                var element = $(this);
-                var myAlign = $(this).position().left;
-                var myWidth = $(this).width()
-                var diff = myScroll+( $(window).height()*.9 );
-
-                if(($(this).position().top) <= diff && $(this).position().top >= ( myScroll-$(this).height())){
-                    if(element.children().hasClass("f")){
-                        element.children().addClass("fi");
-                    }
-                    if(element.children().hasClass("g")){
-                        element.children().addClass("gr");
-                    }
-                    if(element.children().hasClass("afl") || element.children().hasClass("afr"))
-                    {
-                        element.children().addClass("ac");
-                    }
-
-                }else if($(this).hasClass("repeat"))
-                {
-                    outAnimate(element);
-                }
-            });
-        }
-        function outAnimate(element)
-        {
-            if(element.children().hasClass("afl") || element.children().hasClass("afr"))
-            {
-                element.children().removeClass("ac");
-            }
-            if(element.children().hasClass("f")){
-                element.children().removeClass("fi");
-            }
-            if(element.children().hasClass("g")){
-                element.children().removeClass("gr");
-            }
-
-        }
-        initializeAnimation();
-        var myScroll = $(this).scrollTop();
-        inAnimate(myScroll);
-        function graphSize()
-        {
-            /*  console.log("in this bitch");
-            $("#myChart").height("0px");
-            $("#myChart").height($("#description1").height());
-            $("#myChartcon").height($("#description1").height()+10);
-       */ }
-        graphSize();
-        $(window).resize(function(){
-            graphSize();
-        });
-
-        var purchaseMonth = [];
-        var depositMonth = [];
-        var netMonth = [];
-        var combineDP = [];
-      	var minMon = 2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2;
-        $.ajax({
-            url: 'http://api.reimaginebanking.com/accounts/58279be1360f81f104549ddc/purchases?key=92d167a667478cadc9b5542720b5463d',
-            success: function(results){
-
-                for(var i  =0;i<12;i++)
-                {
-                    var sumPurchase = 0;
-                    for(var j =0;j<results.length;j++)
-                    {
-                        if(results[j].purchase_date.indexOf("2015-"+(i+1)+"-")==0 ){
-                            sumPurchase += results[i].amount;
-                        }
-                    }
-                    purchaseMonth.push(sumPurchase*33);
-                    console.log(sumPurchase);
-                }
-                console.log(purchaseMonth);
-                $.ajax({
-                    url: 'http://api.reimaginebanking.com/accounts/58279be1360f81f104549ddc/deposits?key=92d167a667478cadc9b5542720b5463d',
-                    success: function(results){
-
-                        for(var i  =0;i<12;i++)
-                        {
-                            var sumDeposit = 0;
-                            for(var j =0;j<results.length;j++)
-                            {
-                                if(results[j].transaction_date.indexOf("2015-"+(i+1)+"-")==0 ){
-                                    sumDeposit += results[i].amount;
-                                }
-                            }
-                            depositMonth.push(sumDeposit);
-                            console.log(sumDeposit);
-                        }
-                        console.log(depositMonth);
-                        for(var k = 0; k < 12 ; k++)
-                        {
-                          	if(depositMonth[k] < minMon)
-                            {
-                            	minMon = depositMonth[k];
-                            }
-                          	if(purchaseMonth[k] < minMon)
-                            {
-                            	minMon = purchaseMonth[k];
-                            }
-                            netMonth.push(depositMonth[k] - purchaseMonth[k]);
-                        }
-                        combineDP.push(depositMonth);
-                        combineDP.push(purchaseMonth);
-                        makeChart("#myPi",combineDP,minMon);
-
-                    }
-                });
-            }
-        });
-        $.ajax({
-            url: 'http://api.reimaginebanking.com/accounts/58279be1360f81f104549ddc/customer?key=92d167a667478cadc9b5542720b5463d',
-            success: function(results){
-                $("#introduction").html("Welcome "+results.first_name + " " +results.last_name+"!");
-            }
-        });
-        $.ajax({
-            url: 'http://api.reimaginebanking.com/accounts/58279be1360f81f104549ddc?key=92d167a667478cadc9b5542720b5463d',
-            success: function(results){
-                $("#balance").html("Your balance is "+results.balance + " with " +results.rewards+" reward points!");
-            }
-        });
-    </script>
 </html>
